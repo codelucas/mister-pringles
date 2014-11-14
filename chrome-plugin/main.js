3c96b9d584ae58c409c19a9d2e847bf69ffb1fb1
Lucas Ou-Yang <lucasyangpersonal@gmail.com>
2014-11-14 06:21:04 -0800

    Scaffold frontend client

---
+++ chrome-plugin/main.js
@@ -0,0 +1,27 @@
+// Globals
+var prev_length;
+
+console.log('------------loaded plugin------------')
+
+function swap_imgs() {
+    var imgs = $('img').filter(function() {
+        return (parseInt($(this).attr("width")) > 36 &&
+                parseInt($(this).attr("height")) > 36)
+    });
+
+    console.log('----------replacing imgs-----------');
+
+    imgs.each(function(index) {
+        console.log('[page url] ' + $(this).attr('src') );
+        //$.post('http://localhost:8000', {'url': $(this).attr('src')}, function(data) {
+        //    console.log('[post success] ' + data.url); 
+        //}, 'json');
+        $(this).replaceWith(
+            "<img src='http://www.clker.com/cliparts/X/C/L/8/R/Z/red-box-hi.png'>");
+    });
+
+    console.log('LENGTH of images: ' + imgs.length.toString());
+    prev_length = imgs.length;
+}
+
+swap_imgs();
