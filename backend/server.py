94d37377cd918e8abfea9ecdad2772b8e6a27a6b
facebook <facebook@loaner-C02HF01EDYL0.local>
2014-11-14 09:19:09 -0800

    FUCK UUUU

--- backend/server.py
+++ backend/server.py
@@ -4,76 +4,72 @@ import cgi
 import requests
 import numpy as np
 import cv2
+import json
 
 
 PORT_NUMBER = 8000
 global count
 
 count = 0
-face_cascade = cv2.CascadeClassifier('/Users/facebook/mister-pringles/haar/haarcascade_frontalface_default.xml')
+face_cascade = cv2.CascadeClassifier('haar/haarcascade_frontalface_default.xml')
 #face_cascade = cv2.CascadeClassifier('poo')
-eye_cascade = cv2.CascadeClassifier('/Users/facebook/mister-pringles/haar/haarcascade_eye.xml')
-
-
+eye_cascade = cv2.CascadeClassifier('haar/haarcascade_eye.xml')
 
 
 def googlify(filez):
 
-	# print face_cascade
-	# print "\n\n\n\n"
-	# print eye_cascade
 	img = cv2.imread(filez)
 	eye_raw = cv2.imread('eye.png', -1)
 	gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-	#cv2.imshow('img',gray)
-
-	#faces = face_cascade.detectMultiScale(gray, 1.3, 5)
-	faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+	faces = face_cascade.detectMultiScale(gray, 1.3, 3)
 	print len(faces)
 	for (x,y,w,h) in faces:
-		cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),1)
-		
+		# cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),1)
+		print "FOUND FACES" + str(len(faces))
 		roi_gray = gray[y:y+h, x:x+w]
 		roi_color = img[y:y+h, x:x+w]
-		eyes = eye_cascade.detectMultiScale(roi_gray, 1.3, 2)
+		eyes = eye_cascade.detectMultiScale(roi_gray, 1.3, 1)
 		for (ex,ey,ew,eh) in eyes:
-			print ew
-			print end_headers
-			print ex
-			print ey
 			# eye_position = img[ex:ex+ew,ey:ey+eh]
 			eye = cv2.resize(eye_raw, (ew, eh))
 			# eye_array = eye[0:ew,0:eh]
-			print "y:" + str(eye.shape[0])
-			print "x:" + str(eye.shape[1])
+			# print "y:" + str(eye.shape[0])
+			# print "x:" + str(eye.shape[1])
 			y_offset = y + ey
-			x_offset = x + ex;
+			x_offset = x + ex
 			# cv2.addWeighted(eye_position,0,eye_array,1,1,dst)
 			for c in range(0,3):
 				img[y_offset:y_offset+eye.shape[0], x_offset:x_offset+eye.shape[1], c] = eye[:,:,c] * (eye[:,:,3]/255.0) +  img[y_offset:y_offset+eye.shape[0], x_offset:x_offset+eye.shape[1], c] * (1.0 - eye[:,:,3]/255.0)                 
-			#img[y + ey:y + ey+eye.shape[0], x + ex:x + ex+eye.shape[1]] = eye
-			#cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
 
-	# result = cv2.imencode('.jpg',img)
-	cv2.imwrite("pictures/new-" + filez + ".jpg", img)
-	# return result
-	#cv2.imshow('img',img)
+	print "THE TOTAL NAME"  + str (filez)
+	filename = filez + "-new" + ".jpg"
+	cv2.imwrite(filename, img)
+
+	return filename
+
 
 
 class myHandler(BaseHTTPRequestHandler):
+
+
+	def do_GET(self):
+		sep = "/"
+		f = open(self.path[1:])
+		print "THE RETURNED FILE PATH: " + self.path[1:]
+		self.wfile.write(f.read())
+		return
+
 
 	#Handler for the GET requests
 	def do_POST(self):
 		global count
 
 		self.send_response(200)
-		# self.send_header('Content-type','text/html')
 		self.end_headers()
-		# Send the html message
-		self.wfile.write("Hello World !")
-
 		url = self.rfile.read(int(self.headers.getheader('Content-Length')))
+		print "json str is", url
+
 		response = requests.get(url)
 		with open("pictures/" + str(count), 'wb') as fd:
 			for chunk in response.iter_content(1024):
@@ -81,21 +77,14 @@ class myHandler(BaseHTTPRequestHandler):
 
 		fd.close()
 		
-
-		googlify("pictures/" + str(count))
-		self.send_header('Content-type','image/jpg')
-		fp = file("pictures/new-" + str(count) + ".jpg", 'rb')
-		# print img
-		# print type(img)
+		filename = googlify("pictures/" + str(count))
 
 		count = count + 1
 
-		while True:
-			bytes = fp.read(8192)
-			if bytes:
-				self.wfile.write(bytes)
-			else:
-				return
+		self.wfile.write("localhost:8000/" + filename)
+		self.wfile.close()
+
+#googlify('/Users/facebook/Downloads/perfect.jpg')
 
 try:
 	#Create a web server and define the handler to manage the
--- chrome-plugin/main.js
+++ chrome-plugin/main.js
@@ -12,12 +12,14 @@ function swap_imgs() {
     console.log('----------replacing imgs-----------');
 
     imgs.each(function(index) {
+        var $img_tag = $(this);
         console.log('[page url] ' + $(this).attr('src') );
-        $.post('http://localhost:8000', $(this).attr('src'), function(data) {
-            console.log('[post success] ' + data.url); 
-        }, 'json');
-        $(this).replaceWith(
-            "<img src='http://www.clker.com/cliparts/X/C/L/8/R/Z/red-box-hi.png'>");
+        $.post('http://localhost:8000', $(this).attr('src'), function(url) {
+            console.log("DA DA DA");
+            console.log(url);
+            console.log('[post success] ' + url); 
+	        $img_tag.replaceWith("<img src='http://" + url +"'>");
+        });   
     });
 
     console.log('LENGTH of images: ' + imgs.length.toString());
